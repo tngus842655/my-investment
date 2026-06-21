741906584a3595445c48697d4ef37d7a84652b0b
tngus842655 <tngus842655@gmail.com>
2026-06-21 10:51:38 +0900

    최초 커밋

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <h1>MY INVESTMENT</h1>
+
+    <div>
+      <h2>현재 자산</h2>
+      <p>150,000,000원</p>
+    </div>
+
+    <div>
+      <h2>목표 자산</h2>
+      <p>1,000,000,000원</p>
+    </div>
+
+    <div>
+      <h2>월 투자금</h2>
+      <p>5,000,000원</p>
+    </div>
+  </main>
+</template>
